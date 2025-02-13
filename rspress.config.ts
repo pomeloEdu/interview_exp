cb7b4e0b49a95dee17031f3d757a5a2aa8b33f9e
heyongqi10 <heyongqi10@jd.com>
2025-02-13 22:08:05 +0800

    feat: 动态生成

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,41 @@
+import * as fs from 'node:fs';
 import * as path from 'node:path';
+import * as cheerio from 'cheerio';
+import markdownit from 'markdown-it';
 import { defineConfig } from 'rspress/config';
 
+const rootDir = path.join(__dirname, 'docs');
+
+const parseBar = () => {
+  const result = new Array();
+  fs.readdirSync(rootDir).map((file) => {
+    const filePath = path.join(rootDir, file);
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const md = markdownit();
+    const htmlStr = md.render(fileContent);
+    const $ = cheerio.load(htmlStr);
+    const title = $('h1').text();
+    result.push({
+      text: title,
+      link: `/${file.replace('.md', '')}`,
+    });
+  });
+  return result;
+};
+
+const barContent = parseBar();
+
+const chineseText = {
+  lastUpdatedText: '最后更新于',
+  prevPageText: '上一篇',
+  nextPageText: '下一篇',
+  searchPlaceholderText: '搜索文档内容',
+  searchNoResultsText: '未找到',
+  searchSuggestedQueryText: '请尝试修改内容后重新搜索',
+};
+
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
+  root: rootDir,
   title: '柠檬教育的面经合集',
   description: '搜集全网1000+的面经进行整理展示',
   mediumZoom: true,
@@ -14,24 +47,10 @@ export default defineConfig({
   },
   themeConfig: {
     sidebar: {
-      '/': [
-        {
-          text: 'Getting Started',
-          link: '/index',
-        },
-        {
-          text: 'READ',
-          link: '/read',
-        },
-      ],
+      '/': barContent,
     },
     lastUpdated: true,
-    lastUpdatedText: '最后更新于',
-    prevPageText: '上一篇',
-    nextPageText: '下一篇',
-    searchPlaceholderText: '搜索文档内容',
-    searchNoResultsText: '未找到',
-    searchSuggestedQueryText: '请尝试修改内容后重新搜索',
+    ...chineseText,
     socialLinks: [
       {
         icon: 'github',
