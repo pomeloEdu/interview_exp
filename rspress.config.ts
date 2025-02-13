e3d256b147fa9402092d0f7f6339c4a87b729cd7
heyongqi10 <heyongqi10@jd.com>
2025-02-13 21:49:44 +0800

    feat: 完成页面搭建

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  title: '柠檬教育的面经合集',
+  description: '搜集全网1000+的面经进行整理展示',
+  mediumZoom: true,
+  search: {
+    codeBlocks: true,
+  },
+  route: {
+    cleanUrls: true,
+  },
   themeConfig: {
     sidebar: {
       '/': [
@@ -16,5 +25,19 @@ export default defineConfig({
         },
       ],
     },
+    lastUpdated: true,
+    lastUpdatedText: '最后更新于',
+    prevPageText: '上一篇',
+    nextPageText: '下一篇',
+    searchPlaceholderText: '搜索文档内容',
+    searchNoResultsText: '未找到',
+    searchSuggestedQueryText: '请尝试修改内容后重新搜索',
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/pomeloEdu',
+      },
+    ],
   },
 });
